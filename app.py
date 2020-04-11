ca7092343be4f70bdf549236f4b526ed54010066
Awa Kinason <kinason42@gmail.com>
2020-04-11 16:03:14 +0100

    modification of allowed methods on app.py

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ def index():
     return "<h2>Deployment Succeeded</h2>"
 
 
-@app.route('/api/v1/on-covid-19', methods=['POST'], endpoint='estimator')
-@app.route('/api/v1/on-covid-19/json', methods=['POST'], endpoint="estimator")
+@app.route('/api/v1/on-covid-19', methods=['POST', 'GET'], endpoint='estimator')
+@app.route('/api/v1/on-covid-19/json', methods=['POST', 'GET'], endpoint="estimator")
 def estimate_effects():
     """Estimates the effect of COVID-19 based on the data passed."""
     if not request.json:
@@ -51,7 +51,7 @@ def estimate_effects():
     return Response(response=json.dumps(estimator(data)), status=200, content_type='application/json')
 
 
-@app.route('/api/v1/on-covid-19/xml', methods=['POST'], endpoint='estimator_xml')
+@app.route('/api/v1/on-covid-19/xml', methods=['POST', 'GET'], endpoint='estimator_xml')
 def estimate_effects_xml():
     """Estimates the effects of COVID-19 and returns data in XML format."""
     if not request.json:
@@ -68,7 +68,7 @@ def estimate_effects_xml():
     return Response(response=xml, status=200, mimetype='text/xml')
 
 
-@app.route('/api/v1/on-covid-19/logs', methods=['GET'], endpoint='logs')
+@app.route('/api/v1/on-covid-19/logs', methods=['GET', 'POST'], endpoint='logs')
 def extract_logs():
     file = os.path.join(os.path.dirname(__file__), 'src/access.log')
     data = ""
